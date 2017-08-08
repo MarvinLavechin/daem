a2121e566ef37713e6b4949d9024bfd76c93c40c
tbullmann <bullmann.torsten@gmail.com>
2017-08-09 08:49:52 +0900

    first results

---
+++ publication/amount_ground_truth/README.md
@@ -0,0 +1,25 @@
+# Dense reconstruction from electron microscope images
+## Dependency on amount of ground truth
+
+![amount_vs_adapted_RAND](adapted_RAND_amount_ground_truth.jpg)
+
+A residual net with 9 residual layers (= total 24 layers) was used for the generator, trained using the square los.
+The models are trained on 1..16 images, the remaining 19..4 images were used for the test set.
+The above diagrams are created from the F-score (=adapted RAND score) for each image, averaged over the test set. The curve represent a fit with a quadratic function.
+
+There is more variability between the runs than due to the size of the training set.
+
+### Reproduce results
+
+Download the VNC dataset (if necessary)
+
+Run the training, test and aggregate the data
+
+```bash
+bash publication/amount_ground_truth/test_amount.sh
+```
+
+There will be 3 files in the folder `temp/publication/amount_ground_truth/test`: `summary_long.csv`, `summary_wide.scv`, and `averages_wide.csv`.
+
+### Notes
+
---
+++ publication/amount_ground_truth/aggregate.py
@@ -0,0 +1,61 @@
+import os
+import pandas
+import numpy as np
+
+import json
+
+
+def main(test_path='/Volumes/secured/temp/publication/amount_ground_truth/test'):
+    labels = ['membranes', 'synapses', 'mitochondria']
+
+    # concatenate the evaluation and parameters for all runs
+    dfs = []
+    for label in labels:
+        for run in range(1, 16+1):
+            df = read_run_from_csv(test_path, run, label)
+            dfs.append(df)
+    data = pandas.concat(dfs)
+
+    # save aggregated data (in long format)
+    data.to_csv(os.path.join(test_path, 'summary_long.csv'))
+
+    # convert long to wide: label x metric --> label_metric
+    metrics = data.columns.to_series().groupby(data.dtypes).groups[np.dtype('float64')]
+    data2 = data.pivot_table(index=['amount', 'run', 'sample'], columns='label', values=metrics)
+    data2.columns = ['{}_{}'.format(x, y) for x, y in
+                     zip(data2.columns.get_level_values(1), data2.columns.get_level_values(0))]
+    data2 = data2.reset_index()
+
+    # save aggregated data (in wide format)
+    data2.to_csv(os.path.join(test_path, 'summary_wide.csv'))
+
+    # average for each run
+    data = data.groupby(['amount', 'label'])[metrics].mean().reset_index()
+
+    data2 = data.pivot_table(index=['amount'], columns='label', values=metrics)
+    data2.columns = ['{}_{}'.format(x, y) for x, y in
+                     zip(data2.columns.get_level_values(1), data2.columns.get_level_values(0))]
+    data2 = data2.reset_index()
+
+    # save aggregated data (in wide format)
+    data2.to_csv(os.path.join(test_path, 'averages_wide.csv'))
+
+
+def read_run_from_csv(test_path, run, label):
+
+    # path to the test result for a particular model
+    base_path = os.path.join(test_path, '%d' % run)
+
+    # read evaluation results
+    df = pandas.read_csv(os.path.join(base_path, 'evaluation/%s.csv' % label))  # no index_col
+
+    # add parameters
+    df['amount'] = run   # run i uses i images for training set
+    df['label'] = label
+    df['run'] = run
+
+    return df
+
+if __name__ == "__main__":
+    main()
+
