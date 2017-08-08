072fdbd6865c7a84a3956bc37caa35fd5a8f3be8
tbullmann <bullmann.torsten@gmail.com>
2017-08-08 19:47:58 +0900

    first results

---
+++ publication/scaling_resolution/README.md
@@ -0,0 +1,26 @@
+# Dense reconstruction from electron microscope images
+## Dependency on scaling/resolution
+
+![resolution_vs_adapted_RAND](adapted_RAND_resolution.jpg)
+
+A residual net with 9 residual layers (= total 24 layers) was used for the generator, trained using the square los.
+
+A lower resolution (8nm/pixel) can drastically improve synapses recognition, compared with the original resolution (4nm/pixel).
+Interestingly, a higher resolution (2nm/pixel) can lead to complete failure on learning the synapse and mitochondria labels.
+Training of synapse label sometimes failes even on the original resolution.
+
+### Reproduce results
+
+Download the VNC dataset (if necessary)
+
+Run the training, test and aggregate the data
+
+```bash
+bash publication/scaling_resolution/test_scaling.sh
+```
+
+There will be two files in the folder `temp/publication/scaling/test`: `summary_long.csv` and `summary_wide.scv`.
+The above diagrams contain mean and SD for the F-score (=adapted RAND score) for each test image.
+
+### Notes
+
--- publication/scaling_resolution/test_scaling.sh
+++ publication/scaling_resolution/test_scaling.sh
@@ -7,7 +7,10 @@ python imagetranslation/tools/process.py \
   --b_dir datasets/vnc/stack1/labels/ \
   --output_dir temp/publication/scaling/datasets/combined2
 
-for i in 1 2 3 4;
+### Copy combined2 to combined1 and scale 50%
+### Copy combined2 to combined4 and scale 200%
+
+for i in 1 2 4;
 do
 	python imagetranslation/tools/split.py \
 	--dir temp/publication/scaling/datasets/combined$i
@@ -15,7 +18,7 @@ done
 
 
 ### Train
-for i in 1 2 3 4;
+for i in 1 2 4;
 do
 	python imagetranslation/translate.py  --model pix2pix  --mode train  --generator resnet \
 	--output_dir temp/publication/scaling/train/$i \
@@ -25,13 +28,13 @@ do
 	--display_freq 100  --max_epochs 300
 done
 
-### Evaluate training
-for i in 1 2 3 4;
-do
-	bash tools/evaluate.sh temp/publication/scaling/train/$i synapses
-	bash tools/evaluate.sh temp/publication/scaling/train/$i mitochondria
-	bash tools/evaluate.sh temp/publication/scaling/train/$i membranes
-done
+#### Evaluate training
+#for i in 1 2 4;
+#do
+#	bash tools/evaluate.sh temp/publication/scaling/train/$i synapses
+#	bash tools/evaluate.sh temp/publication/scaling/train/$i mitochondria
+#	bash tools/evaluate.sh temp/publication/scaling/train/$i membranes
+#done
 
 
 ### Test and evaluate
@@ -55,22 +58,8 @@ done
 
 
 ### Accumulate results 
-
-# NOTE: Information about generator type / depath can be obtained by order of files only
-
-sed -n 1p temp/publication/scaling/test/1/evaluation/membranes.csv > temp/publication/scaling/evaluation-membranes.csv
-sed -n 1p temp/publication/scaling/test/1/evaluation/mitochondria.csv > temp/publication/scaling/evaluation-mitochondria.csv
-sed -n 1p temp/publication/scaling/test/1/evaluation/synapses.csv > temp/publication/scaling/evaluation-synapses.csv
-for i in 1 2 4;
-do
-	sed 1d temp/publication/scaling/test/$i/evaluation/membranes.csv >> temp/publication/scaling/evaluation-membranes.csv
-	sed 1d temp/publication/scaling/test/$i/evaluation/mitochondria.csv >> temp/publication/scaling/evaluation-mitochondria.csv
-	sed 1d temp/publication/scaling/test/$i/evaluation/synapses.csv >> temp/publication/scaling/evaluation-synapses.csv
-done
-
-
-
-
+# Using the Python script to link evaluation results in CSV files
+pyhton publication/scaling_resolution/aggregate.py
 
 
 
