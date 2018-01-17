dc0723d4624bf64d8867ca3019db149ba4de811e
MarvinLavechin <tsunamyzen@hotmail.fr>
2018-01-17 11:48:57 +0900

    Update random_seed_mode script to make it easier to use for unet

--- scripts/random_seed_mode.sh
+++ scripts/random_seed_mode.sh
@@ -2,8 +2,11 @@
 
 NUMBER_OF_JOBS=$1
 
-for (( c=1; c<=$NUMBER_OF_JOBS; c++ ))
+for (( n=1; n<=8; n++))
 do
-	sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --generator unet --u_depth 8 --max_epochs 1000 --random_seed_mode
-	sleep 1
+	for (( c=1; c<=$NUMBER_OF_JOBS; c++ ))
+	do
+		echo "sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --generator unet --u_depth $n --max_epochs 1000 --random_seed_mode"
+		sleep 1
+	done
 done
