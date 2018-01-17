46d4736924f1c68b43d77a13e0fbfc59497cb7a2
Marvin Lavechin <lavechin-m@knv00>
2018-01-17 11:16:24 +0900

    Modification random seed mode

--- scripts/random_seed_mode.sh
+++ scripts/random_seed_mode.sh
@@ -4,6 +4,6 @@ NUMBER_OF_JOBS=$1
 
 for (( c=1; c<=$NUMBER_OF_JOBS; c++ ))
 do
-	sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --generator unet --max_epochs 1000 --random_seed_mode
+	sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --generator unet --u_depth 8 --max_epochs 1000 --random_seed_mode
 	sleep 1
 done
