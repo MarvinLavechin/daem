80fcf4797cab6c501b612e16d38bfa0c9ecde1a7
Marvin Lavechin <lavechin-m@knv00>
2018-01-17 12:07:35 +0900

    Remove echo from script

--- scripts/random_seed_mode.sh
+++ scripts/random_seed_mode.sh
@@ -6,7 +6,7 @@ for (( n=1; n<=8; n++))
 do
 	for (( c=1; c<=$NUMBER_OF_JOBS; c++ ))
 	do
-		echo "sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --generator unet --u_depth $n --max_epochs 1000 --random_seed_mode"
+		sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --generator unet --u_depth $n --max_epochs 1000 --random_seed_mode
 		sleep 1
 	done
 done
