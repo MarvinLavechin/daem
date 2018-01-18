f0bd8d76e23aa53e9915f83be4fc04f686932a51
Marvin Lavechin <marvin.lavechin@ensimag.grenoble-inp.fr>
2018-01-18 18:47:52 +0900

    Correction bash script if condition

--- scripts/train_apply_cycleGAN_rawraw.sh
+++ scripts/train_apply_cycleGAN_rawraw.sh
@@ -286,13 +286,13 @@ NEW_HTML_FILE=temp/Example_Transfer_RawRaw/"$average"summary"$SUFFIX_NAME".html
 mv $HTML_FILE $NEW_HTML_FILE
 
 #We save the html file, and its pdf version if everything has been executed correctly.
-if [ -f temp/Example_Transfer_RawRaw/test_da_seg/$NAME_TEST/images/49-inputs.png ] && [ -f temp/Example_Transfer_RawRaw/test_da_seg/$NAME_TEST/images/49_translated-inputs.png ] && [ $SCORE_MEMBRANE != 0 ]; then
+if [ -f "temp/Example_Transfer_RawRaw/test_da_seg/$NAME_TEST/images/49-inputs.png" ] && [ -f "temp/Example_Transfer_RawRaw/test_da_seg/$NAME_TEST/images/49_translated-inputs.png" ] && [ $error_membrane != 0 ]; then
     wkhtmltopdf -O landscape $NEW_HTML_FILE ~/Documents/"$average"summary"$SUFFIX_NAME".pdf
     mkdir -p ~/Documents_html
     mv $NEW_HTML_FILE ~/Documents_html
 fi
 
 #We remove temporary files to avoid to run out of memory
-rm $OUTPUT_DIR
-rm $OUTPUT_SEGMENTATION_TRANSLATED
+rm -rf $OUTPUT_DIR
+rm -rf $OUTPUT_SEGMENTATION_TRANSLATED
 
