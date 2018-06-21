38fda0a593cf3f335e2a610d226f46423362697e
MarvinLavechin <tsunamyzen@hotmail.fr>
2018-06-21 17:21:58 +0900

    Update random_script_mode to handle several values of the weight segmentation loss

--- scripts/random_seed_mode.sh
+++ scripts/random_seed_mode.sh
@@ -2,10 +2,11 @@
 
 NUMBER_OF_JOBS=$1
 
-
+for l in 1 10 100 100; do
     for (( c=1; c<=$NUMBER_OF_JOBS; c++ ))
     do
-        sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --max_epochs 2000 --generator unet --u_depth 2 --random_seed_mode
+        sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --checkpoint_segmentation temp/Example_2D_3Labels/train_on_cropped --max_epochs 1000 --weight_segmentation $l --generator unet --u_depth 2 --random_seed_mode
         sleep 1
     done
+done
 
