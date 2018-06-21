d8509f1cc53076c531517083af656f5941527e36
MarvinLavechin <tsunamyzen@hotmail.fr>
2018-06-21 17:26:19 +0900

    Erratta random script mode

--- scripts/random_seed_mode.sh
+++ scripts/random_seed_mode.sh
@@ -2,7 +2,7 @@
 
 NUMBER_OF_JOBS=$1
 
-for l in 1 10 100 100; do
+for l in 1 10 100 1000; do
     for (( c=1; c<=$NUMBER_OF_JOBS; c++ ))
     do
         sbatch -p gpucpuM train_apply_cycleGAN_rawraw.sh --checkpoint_segmentation temp/Example_2D_3Labels/train_on_cropped --max_epochs 1000 --weight_segmentation $l --generator unet --u_depth 2 --random_seed_mode
